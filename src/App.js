e7608cccec695999581ae6e900f45eae2db21119
colorkite10 <colorkite10@gmail.com>
2024-07-30 08:53:20 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  const effectFn = () => {
-    console.log("created");
-    return destroyedFn;
-  };
-
-  const destroyedFn = () => {
-    console.log("destroyed");
-  };
-
-  useEffect(effectFn, []);
-  return <h1>hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
