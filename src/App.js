297bbbae8e470e364d4be188a1b14659b07a2ba2
colorkite10 <colorkite10@gmail.com>
2024-07-30 08:48:51 +0900

    cleanup 함수는 컴포넌트가 destroyed될 때 실행된다

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import { useEffect, useState } from "react";
 
+function Hello() {
+  const effectFn = () => {
+    console.log("created");
+    return destroyedFn;
+  };
+
+  const destroyedFn = () => {
+    console.log("destroyed");
+  };
+
+  useEffect(effectFn, []);
+  return <h1>hello</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setCounter((current) => current + 1);
-
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("i run all the time");
-
-  useEffect(() => {
-    console.log("i run only once");
-  }, []); //최초 한 번만 실행됨
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("i run when keyword changes");
-    }
-  }, [keyword]); //keyword가 변화할 때마다 실행됨
-
-  useEffect(() => {
-    console.log("i run when counter changes");
-  }, [counter]); //counter가 변화할 때마다 실행됨
-
-  useEffect(() => {
-    console.log("i run when keyword or counter change");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        type="text"
-        value={keyword}
-        onChange={onChange}
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
