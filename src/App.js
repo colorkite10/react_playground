666f006ee09a8966e05dfae8796c7451fa526122
colorkite10 <colorkite10@gmail.com>
2024-07-29 14:40:08 +0900

    useEffect는 최초 렌더링 시 한 번만 수행된다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const onClick = () => setCounter((current) => current + 1);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
+
   return (
     <div>
-      <h1>welcome back</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
