1e3e0dd6fb5da10f3ad3bad44353091713378e92
colorkite10 <colorkite10@gmail.com>
2024-07-30 10:35:59 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,53 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState(0);
-  const [getCoin, setGetCoin] = useState(0);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => setCoins(json));
-    setLoading(false);
-  }, []);
-
-  const writeMoney = (e) => setMyMoney(e.target.value);
-  const selectCoin = (e) => setGetCoin(e.target.value);
-
-  return (
-    <div>
-      <h1>the coins {loading ? "" : `(${coins.length})`}</h1>
-      <div>
-        <input
-          type="number"
-          value={myMoney}
-          onChange={writeMoney}
-          placeholder="WRITE UR USD"
-        ></input>
-      </div>
-      {loading ? (
-        <div>loading...</div>
-      ) : (
-        <select onChange={selectCoin}>
-          <option key="-1">select coin</option>
-          {coins.map((coin) => (
-            <option key={coin.id} value={coin.quotes.USD.price}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price.toFixed(2)}{" "}
-              USD
-            </option>
-          ))}
-        </select>
-      )}
-      <div>
-        <h2>
-          Coins you can buy:{" "}
-          {getCoin > 0 ? (myMoney / getCoin).toFixed(2) : null}
-        </h2>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
