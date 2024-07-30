74914cc32d6856f79552228d117c229dcfdd8b1d
colorkite10 <colorkite10@gmail.com>
2024-07-30 09:16:58 +0900

    todo 만들기(1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => setTodo(event.target.value);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+
+  return (
+    <div>
+      <h1>My Todos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="투두 작성하세요"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
