ddbabf1f4e2dc92f7f746a6f479e2f45ac867699
colorkite10 <colorkite10@gmail.com>
2024-07-30 10:31:35 +0900

    input에 입력한 돈으로 선택한 코인을 몇 개 살 수 있는지 보여주기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState(0);
+  const [getCoin, setGetCoin] = useState(0);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -11,20 +13,39 @@ function App() {
     setLoading(false);
   }, []);
 
+  const writeMoney = (e) => setMyMoney(e.target.value);
+  const selectCoin = (e) => setGetCoin(e.target.value);
+
   return (
     <div>
-      <h1>the coins {loading ? "" : `({coins.length})`}</h1>
+      <h1>the coins {loading ? "" : `(${coins.length})`}</h1>
+      <div>
+        <input
+          type="number"
+          value={myMoney}
+          onChange={writeMoney}
+          placeholder="WRITE UR USD"
+        ></input>
+      </div>
       {loading ? (
         <div>loading...</div>
       ) : (
-        <select>
+        <select onChange={selectCoin}>
+          <option key="-1">select coin</option>
           {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price.toFixed(2)}{" "}
+              USD
             </option>
           ))}
         </select>
       )}
+      <div>
+        <h2>
+          Coins you can buy:{" "}
+          {getCoin > 0 ? (myMoney / getCoin).toFixed(2) : null}
+        </h2>
+      </div>
     </div>
   );
 }
