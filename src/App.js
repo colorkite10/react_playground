a089826c2d7317c8e15718977313ef9c3e4233fb
colorkite10 <colorkite10@gmail.com>
2024-07-30 10:06:01 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const onChange = (event) => setTodo(event.target.value);
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
-  };
-
-  return (
-    <div>
-      <h1>My Todos ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholder="투두 작성하세요"
-        />
-        <button>Add</button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
